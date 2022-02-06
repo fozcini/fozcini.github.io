0ae383fd072439e610cd310687eb45301a92ff7f
fozcini <f.ozcini@outlook.com>
2022-02-07 00:20:01 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -1,39 +1,37 @@
-window.onload = function(e) {
-    function loadJSON(callback) {
-        var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-        xobj.open('GET', '/data/data.json', true);
-        xobj.onreadystatechange = function() {
-            if (xobj.readyState == 4 && xobj.status == "200") {
-                callback(xobj.responseText);
-            }
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '/data/data.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
         }
-        xobj.send(null);
     }
-
-    loadJSON(function(response) {
-        jsonresponse = JSON.parse(response);
-        let id = new URLSearchParams(document.location.search).get("id")
-        jsonresponse.forEach(e => {
-            if (e.id == id) {
-
-                let phone = "tel:" + e.Phone
-                let mail = "mailto:" + e.Email
-
-                document.getElementById("linkedin-link").href = e.LinkedIn;
-                document.getElementById("whatsapp-link").href = e.Whatsapp;
-                document.getElementById("profile-photo").src = "/images/profile/" + e.id + ".png";
-                document.getElementById("name").innerHTML = e.Name;
-                document.getElementById("title").innerHTML = e.Title;
-                document.getElementById("phone-call").href = phone;
-                document.getElementById("map-inner").src = e.Map
-                document.getElementById("address").innerHTML = e.Address
-                document.getElementById("email").href = mail
-                document.getElementById("email").innerHTML = e.Email
-                document.getElementById("phone").href = phone
-                document.getElementById("phone").innerHTML = e.Phone
-                document.getElementById("office").innerHTML = e.Office
-            }
-        });
-    });
+    xobj.send(null);
 }
+
+loadJSON(function(response) {
+    jsonresponse = JSON.parse(response);
+    let id = new URLSearchParams(document.location.search).get("id")
+    jsonresponse.forEach(e => {
+        if (e.id == id) {
+
+            let phone = "tel:" + e.Phone
+            let mail = "mailto:" + e.Email
+
+            document.getElementById("linkedin-link").href = e.LinkedIn;
+            document.getElementById("whatsapp-link").href = e.Whatsapp;
+            document.getElementById("profile-photo").src = "/images/profile/" + e.id + ".png";
+            document.getElementById("name").innerHTML = e.Name;
+            document.getElementById("title").innerHTML = e.Title;
+            document.getElementById("phone-call").href = phone;
+            document.getElementById("map-inner").src = e.Map
+            document.getElementById("address").innerHTML = e.Address
+            document.getElementById("email").href = mail
+            document.getElementById("email").innerHTML = e.Email
+            document.getElementById("phone").href = phone
+            document.getElementById("phone").innerHTML = e.Phone
+            document.getElementById("office").innerHTML = e.Office
+        }
+    });
+});
