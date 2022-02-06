b4182e97617ba4d337fe489df1f7a53636225c8d
fozcini <f.ozcini@outlook.com>
2022-02-06 22:58:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,28 +17,20 @@ loadJSON(function(response) {
     jsonresponse.forEach(e => {
         if (e.id == id) {
 
-            console.log(e.LinkedIn)
-            console.log(e.Whatsapp)
-            console.log(e.id)
-            console.log(e.Name)
-            console.log(e.Title)
-            console.log(e.Map)
-            console.log(e.Address)
-            console.log(e.Email)
-            console.log(e.Phone)
-            console.log(e.Office)
+            let phone = "tel:" + e.Phone
+            let mail = "mailto:" + e.Email
 
             document.getElementById("linkedin-link").href = e.LinkedIn;
             document.getElementById("whatsapp-link").href = e.Whatsapp;
             document.getElementById("profile-photo").src = "/images/profile/" + e.id + ".png";
             document.getElementById("name").innerHTML = e.Name;
             document.getElementById("title").innerHTML = e.Title;
-            document.getElementById("phone-call").href = "tel:" + e.Phone;
+            document.getElementById("phone-call").href = phone;
             document.getElementById("map-inner").src = e.Map
             document.getElementById("address").innerHTML = e.Address
-            document.getElementById("email").href = "mailto:" + e.Email
+            document.getElementById("email").href = mail
             document.getElementById("email").innerHTML = e.Email
-            document.getElementById("phone").href = "tel:" + e.Phone
+            document.getElementById("phone").href = phone
             document.getElementById("phone").innerHTML = e.Phone
             document.getElementById("office").innerHTML = e.Office
         }
